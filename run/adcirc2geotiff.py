f7af42ac41a9d2b2447d0a706b1176fe6353b7fa
James McManus <jmpmcman@renci.org>
2022-06-09 14:28:33 -0400

    Added revisions for producing image mosaics

--- run/adcirc2geotiff.py
+++ run/adcirc2geotiff.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # Import Python modules
-import os, sys, argparse, shutil, json, warnings
+import os, sys, argparse, shutil, json, warnings, pdb
 import netCDF4 as nc
+import numpy as np
+from datetime import datetime
+from datetime import timedelta
 from loguru import logger
 from functools import wraps
 
@@ -16,6 +19,7 @@ from qgis.core import (
     QgsProject,
     QgsRasterLayer,
     QgsRasterFileWriter,
+    QgsRectangle,
     QgsErrorMessage
 )
 
@@ -31,8 +35,8 @@ def ignore_warnings(f):
 
 # Initialize application
 def initialize_qgis_application():
-    sys.path.append('/opt/conda/envs/adcirc2geotiff/share/qgis')
-    sys.path.append('/opt/conda/envs/adcirc2geotiff/share/qgis/python/plugins')
+    sys.path.append('/home/nru/.conda/envs/adcirc2cog/share/qgis')
+    sys.path.append('/home/nru/.conda/envs/adcirc2cog/share/qgis/python/plugins')
     app = QgsApplication([], False)
     return (app)
 
@@ -51,97 +55,203 @@ def initialize_processing(app):
 def makeDirs(outputDir):
     # Create cogeo directory path
     if not os.path.exists(outputDir):
-        # mode = 0o755
-        # os.makedirs(outputDir, mode)
-        os.makedirs(outputDir, exist_ok=True)
+        mode = 0o777
+        os.makedirs(outputDir, mode, exist_ok=True)
         logger.info('Made directory '+outputDir+ '.')
     else:
         logger.info('Directory '+outputDir+' already made.')
 
 # Define parameters used in creating tiff
-def getParameters(inputDir, inputFile, outputDir):
-    inputFileList = inputFile.split('.')
-    inputFileList.insert(1,'raw')
-    inputFileList[-1] = 'tif'
-    tiffFile = ".".join(inputFileList)
-    parms = '{"INPUT_EXTENT" : "-97.85833,-60.040029999999994,7.909559999999999,45.83612", "INPUT_GROUP" : 1, "INPUT_LAYER" : "'+inputDir+inputFile+'", "INPUT_TIMESTEP" : 0,  "OUTPUT_RASTER" : "'+outputDir+tiffFile+'", "MAP_UNITS_PER_PIXEL" : 0.005}'
+def getParameters(inputDir, inputFile, outputDir, outputFile, inputExtent, timeStep, mapUnitPP):
+    parms = '{"INPUT_EXTENT" : "'+inputExtent+'", "INPUT_GROUP" : 1, "INPUT_LAYER" : "'+inputDir+inputFile+'", "INPUT_TIMESTEP" : '+str(timeStep)+',  "OUTPUT_RASTER" : "'+outputDir+outputFile+'", "MAP_UNITS_PER_PIXEL" : '+str(mapUnitPP)+'}'
     return(json.loads(parms))
 
-# Convert mesh layer as raster and save as a GeoTiff
 @ignore_warnings
-def exportRaster(parameters):
-    # Open layer from INPUT_LAYER
-    logger.info('Open layer from INPUT_LAYER')
-    inputFile = 'Ugrid:'+'"'+parameters['INPUT_LAYER']+'"'
-    meshfile = inputFile.strip().split('/')[-1]
-    meshlayer = meshfile.split('.')[0]
-    layer = QgsMeshLayer(inputFile, meshlayer, 'mdal')
+class mesh2tiff:
+    def __init__(self, inputDir, outputDir, inputFile, tmpDir):
+        # Open layer from INPUT_LAYER
+        logger.info('Open layer from input '+inputDir+inputFile+' file.')
+        inputMeshFile = 'Ugrid:'+'"'+inputDir+inputFile+'"'
+        meshFile = inputFile.strip().split('/')[-1]
+        meshLayer = meshFile.split('.')[0]
+        self.layer = QgsMeshLayer(inputMeshFile, meshLayer, 'mdal')
 
-    # Open INPUT_LAYER with netCDF4, and check its dimensions. If dimensions are incorrect exit program
-    logger.info('Check INPUT_LAYER dimensions')
-    ds = nc.Dataset(parameters['INPUT_LAYER'])
-    for dim in ds.dimensions.values():
-        if dim.size == 0:
-            logger.info('The netCDF file '+meshfile.split('"')[0]+' has an invalid dimension value of 0, so the program will exit')
-            sys.exit(0)
+        # Define self.tmpDir
+        self.tmpDir = tmpDir
 
-    # Check if layer is valid
-    if layer.isValid() is True:
-        # Get parameters for processing
-        logger.info('Get parameters')
-        dataset  = parameters['INPUT_GROUP'] 
-        timestep = parameters['INPUT_TIMESTEP']
-        mupp = parameters['MAP_UNITS_PER_PIXEL'] 
-        extent = layer.extent()
-        output_layer = parameters['OUTPUT_RASTER']
-        width = extent.width()/mupp 
-        height = extent.height()/mupp 
-        crs = layer.crs() 
-        crs.createFromSrid(4326)
+        # Open INPUT_LAYER with netCDF4, and check its dimensions. If dimensions are incorrect exit program
+        logger.info('Check INPUT_LAYER '+inputDir+inputFile+' dimensions')
+        ds = nc.Dataset(inputDir+inputFile)
+        times = list(ds.variables['time'][:].data)
+        year, month, day = ds.variables['time'].base_date.split(' ')[0].split('-')
+        hour, minute, second = ds.variables['time'].base_date.split(' ')[1].split(':')
+        base_date = datetime(int(year), int(month), int(day), int(hour), int(minute), int(second)) 
+        for dim in ds.dimensions.values():
+            if dim.size == 0:
+                logger.info('The netCDF file '+meshFile.split('"')[0]+' has an invalid dimension value of 0, so the program will exit')
+                sys.exit(1)
+        ds.close()
 
-        # Transform instance
-        logger.info('Transform instance')
-        transform_context = QgsProject.instance().transformContext()
-        output_format = QgsRasterFileWriter.driverForExtension(os.path.splitext(output_layer)[1])
+        # Check times length, if time equals 1 then data is a max file, if time greater than 1 then data is a timeseries file
+        if len(times) == 1:
+            # Define timestep
+            timeStep = 0
 
-        # Open output file for writing
-        logger.info('Open output file')
-        rfw = QgsRasterFileWriter(output_layer)
-        rfw.setOutputProviderKey('gdal') 
-        rfw.setOutputFormat(output_format) 
+            # Define map units per pixel
+            mapUnitsPP = [0.005, 0.001, 0.001, 0.005, 0.005, 0.005, 0.005, 0.005]
 
-        # Create one band raster
-        logger.info('Create one band raster')
-        rdp = rfw.createOneBandRaster( Qgis.Float64, width, height, extent, crs)
+            # Define fileDateTime
+            fileDateTime = datetime.fromisoformat(str(base_date + timedelta(seconds=times[0]))).strftime("%Y%m%dT%H%M%S")
+ 
+            # Define input extent parameters to create tiff from ADCIRC mesh file
+            inputExtents = ['-77.85833,-60.040029999999994,36.0,45.83612',
+                            '-97.85833,-81.0,23.0,31.273088',
+                            '-81.937856,-74.0,23.0,36.555922',
+                            '-74.467152,-60.040029999999994,23.0,36.83612',
+                            '-97.85833,-77.0,15.0,23.213514',
+                            '-77.85833,-60.040029999999994,15.0,23.213514',
+                            '-97.85833,-77.0,7.909559999999999,15.83612',
+                            '-77.85833,-60.040029999999994,7.909559999999999,15.83612']
 
-        # Get dataset index
-        logger.info('Get data set index')
-        dataset_index = QgsMeshDatasetIndex(dataset, timestep)
+            # Define input_list and outputFile index
+            inputs_list = []
+            i = 0
 
-        # Regred mesh layer to raster
-        logger.info('Regrid mesh layer')
-        block = QgsMeshUtils.exportRasterBlock( layer, dataset_index, crs,
-                transform_context, mupp, extent) 
+            # Add variables to input_list
+            logger.info('Create inputs_list, which has multiple extents, and one timeStep')
+            for inputExtent in inputExtents:
+                inputFileList = inputFile.split('.')
+                inputFileList.insert(1,'raw')
+                inputFileList.insert(1,'subset'+str(i))
+                inputFileList[-1] = 'tif'
+                outputFile = ".".join(inputFileList)
+                mapUnitPP = mapUnitsPP[i]
 
-        # Write raster to GeoTiff file
-        logger.info('Write raster Geotiff file')
-        rdp.writeBlock(block, 1)
-        rdp.setNoDataValue(1, block.noDataValue())
-        rdp.setEditable(False)
+                inputs_list.append([inputDir, inputFile, outputDir, outputFile, inputExtent, timeStep, mapUnitPP])
 
-        logger.info('Regridded mesh data in '+meshfile.split('"')[0]+' to float64 grid, and saved to tiff ('+output_layer.split('/')[-1]+') file.')
+                i = i + 1
 
-        return(output_layer)
+        elif len(times) > 1:
+            # Define timeSteps
+            timeSteps = list(range(len(times)))
 
-    if layer.isValid() is False: 
-        raise Exception('Invalid mesh')
+            # Define map units per pixel
+            mapUnitPP = 0.005
+
+            # Define input extent parameters to create tiff from ADCIRC mesh file
+            #inputExtent = '-97.85833,-60.040029999999994,7.909559999999999,45.83612'
+            inputExtent = '-97.85833,-68.0,24.0,42.0'
+            #inputExtents = ['-97.85833,-68.0,24.0,31.360610', '-82.0,-68.0,31.0,42.0']
+
+            # Define input_list times index
+            inputs_list = []
+            i = 0
+
+            # Add variables to input_lists
+            logger.info('Create inputs_list, which has one extent, and multiple timeStep')
+            for timeStep in timeSteps:
+                # Define fileDateTime
+                fileDateTime = datetime.fromisoformat(str(base_date + timedelta(seconds=times[i]))).strftime("%Y%m%dT%H%M%S")
+
+                inputFileList = inputFile.split('.')
+                inputFileList.insert(2,'raw')
+                inputFileList.insert(2,fileDateTime)
+                inputFileList[-1] = 'tif'
+                outputFile = ".".join(inputFileList)
+
+                inputs_list.append([inputDir, inputFile, outputDir, outputFile, inputExtent, timeStep, mapUnitPP])
+
+                i = i + 1
+
+        else:
+            logger.info('Incorrect times length')
+            sys.exit(1)
+
+        # Run exportRaster using multiprocessinng for loop, and imput_list
+        logger.info('Run exportRaster in for loop, with inputs_list')
+        for inputList in inputs_list:
+           self.exportRaster(inputList) 
+
+        self.layer = None
+
+    # Convert mesh layer as raster and save as a GeoTiff
+    def exportRaster(self, inputList):
+        # Get input from inputList
+        inputDir = inputList[0]
+        inputFile = inputList[1]
+        outputDir = inputList[2]
+        outputFile = inputList[3]
+        inputExtent = inputList[4]
+        timeStep = inputList[5]
+        mapUnitPP = inputList[6]
+
+        # Get parameters
+        parameters = getParameters(inputDir, inputFile, outputDir, outputFile, inputExtent, timeStep, mapUnitPP)
+
+        # Check if layer is valid
+        if self.layer.isValid() is True:
+            # Get parameters for processing
+            logger.info('Get parameters for '+parameters['INPUT_LAYER']+'.')
+            dataset  = parameters['INPUT_GROUP'] 
+            timestep = parameters['INPUT_TIMESTEP']
+            mupp = parameters['MAP_UNITS_PER_PIXEL'] 
+            input_extent = parameters['INPUT_EXTENT'].split(',')
+            extent = QgsRectangle(float(input_extent[0]),float(input_extent[2]),float(input_extent[1]),float(input_extent[3]))
+            output_layer = parameters['OUTPUT_RASTER']
+            width = extent.width()/mupp 
+            height = extent.height()/mupp 
+            crs = self.layer.crs() 
+            crs.createFromSrid(4326)
+
+            # Transform instance
+            logger.info('Transform instance of '+parameters['INPUT_LAYER']+'.')
+            transform_context = QgsProject.instance().transformContext()
+            output_format = QgsRasterFileWriter.driverForExtension(os.path.splitext(output_layer)[1])
+
+            # Open output file for writing
+            logger.info('Open output file '+output_layer+'.')
+            rfw = QgsRasterFileWriter(output_layer)
+            rfw.setOutputProviderKey('gdal') 
+            rfw.setOutputFormat(output_format) 
+
+            # Create one band raster
+            logger.info('Create one band raster')
+            rdp = rfw.createOneBandRaster( Qgis.Float64, width, height, extent, crs)
+
+            # Get dataset index
+            logger.info('Get data set index')
+            dataset_index = QgsMeshDatasetIndex(dataset, timestep)
+
+            # Regred mesh layer to raster
+            logger.info('Regrid mesh layer '+inputDir+inputFile+'.' )
+            os.chdir(self.tmpDir)
+            block = QgsMeshUtils.exportRasterBlock( self.layer, dataset_index, crs,
+                    transform_context, mupp, extent) 
+            os.chdir('/home/nru/repos/adcirc2cog/run')
+
+            # Write raster to GeoTiff file
+            logger.info('Write raster Geotiff ('+output_layer+') file.')
+            rdp.writeBlock(block, 1)
+            rdp.setNoDataValue(1, block.noDataValue())
+            rdp.setEditable(False)
+
+            block = None
+
+            logger.info('Regridded mesh data in '+inputDir+inputFile+' to float64 grid, and saved to tiff ('+output_layer+') file.')
+
+        if self.layer.isValid() is False: 
+            raise Exception('Invalid mesh ('+inputDir+inputFile+') file.')
 
 @logger.catch
 def main(args):
     # get input variables from args
-    inputFile = args.inputFile
     inputDir = os.path.join(args.inputDir, '')
     outputDir = os.path.join(args.outputDir, '')
+    inputFile = args.inputFile
+    outputDir = os.path.join(outputDir+"".join(inputFile[:-3].split('.')), '')
+
+    # Define tmp directory
+    tmpDir = "/".join(inputDir.split("/")[:-2])+"/"+inputFile.split('.')[0]+"_qgis_tmp/"
 
     # Remove old logger and start new one
     logger.remove()
@@ -161,7 +271,19 @@ def main(args):
         xdg_runtime_dir = '/home/nru/adcirc2geotiff'
         os.makedirs(xdg_runtime_dir, exist_ok=True)
         os.environ['XDG_RUNTIME_DIR']=xdg_runtime_dir
+        os.makedirs(tmpDir, exist_ok=True)
+        os.environ['TMPDIR'] = tmpDir
         logger.info('Set QGIS enviroment.')
+
+        # Check if tmpDir exists
+        if not os.path.exists(tmpDir):
+            logger.error('The tmpDir: '+tmpDir+' does not exist')
+            sys.exit(1)
+        elif os.path.exists(tmpDir):
+            logger.info('The tmpDir: '+tmpDir+' does exist')
+        else:
+            logger.error('Checked for tmpDir: '+tmpDir+', and else statement happened')
+            sys.exit(1)
 
         # Initialize QGIS
         app = initialize_qgis_application() 
@@ -169,12 +291,8 @@ def main(args):
         app, processing = initialize_processing(app)
         logger.info('Initialzed QGIS.')
 
-        # get parameters to create tiff from ADCIRC mesh file
-        parameters = getParameters(inputDir, inputFile.strip(), outputDir.strip())
-        logger.info('Got mesh regrid paramters for '+inputFile.strip())
-
-        # Create raw tiff file 
-        filename = exportRaster(parameters)
+        # Run mesh2tiff and producer tiff files
+        mesh2tiff(inputDir, outputDir, inputFile, tmpDir)
 
         # Quit QGIS
         app.exitQgis()
@@ -182,17 +300,16 @@ def main(args):
 
     else:
          logger.info(inputFile+' does not exist')
-         sys.exit(0)
+         sys.exit(1)
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
     parser = argparse.ArgumentParser()
 
     # Optional argument which requires a parameter (eg. -d test)
-    parser.add_argument("--inputFILE", "--inputFile", help="Input file name", action="store", dest="inputFile", required=True)
     parser.add_argument("--inputDIR", "--inputDir", help="Input directory path", action="store", dest="inputDir", required=True)
     parser.add_argument("--outputDIR", "--outputDir", help="Output directory path", action="store", dest="outputDir", required=True)
-
+    parser.add_argument("--inputFILE", "--inputFile", help="Input file name", action="store", dest="inputFile", required=True)
     args = parser.parse_args()
     main(args)
 
--- run/geotiff2cog.py
+++ run/geotiff2cog.py
@@ -1,95 +1,179 @@
 #!/usr/bin/env python
-import sys, os, argparse, shutil
+import sys, os, argparse, shutil, glob
 from pathlib import Path
 from loguru import logger
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, STDOUT
+from multiprocessing.pool import ThreadPool as Pool
 
-def geotiff2cog(inputFile, inputDir, finalDir):
-    # Create cog directory path
-    if not os.path.exists(inputDir):
-        #mode = 0o755
-        #os.makedirs(inutDir, mode)
-        os.makedirs(inputDir, exist_ok=True)
-        logger.info('Made directory '+Path(inputDir).parts[-1]+ '.')
-    else:
-        logger.info('Directory '+Path(inputDir).parts[-1]+' already made.')
+# Function creates a process using command from cmd
+def call_proc(cmd):
+    # This runs in a separate thread
+    p = Popen(cmd, stdout=PIPE, stderr=STDOUT)
+    stdout, stderr = p.communicate()
+    return (stdout, stderr)
 
-    # Define input tiff file name and path
-    tiffFile = inputDir+inputFile
+def geotiff2cog(inputDir, finalDir):
+    # Create empty list for commands
+    cmds_list = []
 
-    # Define ouput cog file name
-    inputFileList = inputFile.split('.')
-    inputFileList.insert(-1,'cog')
-    outputFile = ".".join(inputFileList)
+    # Get list of input tiff files
+    for inputPathFile in glob.glob(inputDir+'*.tif'):
 
-    # Remove cog file if it already exits
-    if os.path.exists(inputDir+outputFile):
-        os.remove(inputDir+outputFile)
-        logger.info('Removed old cog file '+inputDir+outputFile+'.')
-        logger.info('Cogeo path '+inputDir+outputFile+'.')
-    else:
-        logger.info('Cogeo path '+inputDir+outputFile+'.')
+        # Check if input file exists and if it does run geotiff2cog function
+        if os.path.exists(inputPathFile):
+            # When error exit program
+            logger.add(lambda _: sys.exit(1), level="ERROR")
 
-    # Difine command to create cog
-    cmds_list = [
-      ['rio', 'cogeo', 'create',  tiffFile, inputDir+outputFile, '--web-optimized']
-    ]
+            logger.info('Create cog file from '+inputPathFile.strip()+' tiff file.')
 
-    # Define process list of commands
-    procs_list = [Popen(cmd, stdout=PIPE, stderr=PIPE) for cmd in cmds_list]
+            # Define ouput cog file name
+            inputFileList = inputPathFile.split('/')[-1].split('.')
+            inputFileList.insert(-1,'cog')
+            outputFile = ".".join(inputFileList)
 
-    # Run process list of commands
-    for proc in procs_list:
-        proc.wait()
+            # Remove cog file if it already exits
+            if os.path.exists(inputDir+outputFile):
+                os.remove(inputDir+outputFile)
+                logger.info('Removed old cog file '+inputDir+outputFile+'.')
+                logger.info('Cogeo path '+inputDir+outputFile+'.')
+            else:
+                logger.info('Cogeo path '+inputDir+outputFile+'.')
 
-    logger.info('Created cog file '+outputFile+' from tiff file '+inputFile+'.')
+            # Difine command to create cog
+            cmds_list.append(['rio', 'cogeo', 'create',  inputPathFile, inputDir+outputFile, '--web-optimized'])
+
+        else:
+            logger.info(inputPathFile+' does not exist')
+            sys.exit(1)
+
+    # Define number of CPU to use in pool.
+    pool = Pool(processes=4)
+
+    # Apply cmds_list to pool, and output to resutls
+    results = []
+    for cmd in cmds_list:
+        results.append(pool.apply_async(call_proc, (cmd,)))
+
+    # Close the pool and wait for each running task to complete
+    pool.close()
+    pool.join()
+
+    # Output results to log
+    for result in results:
+        stdout, stderr = result.get()
+
+        if stderr:
+            logger.info("stdout: {} stderr: {}".format(stdout, stderr))
+            sys.exit(1)
+        else:
+            logger.info("stdout: {} stderr: {}".format(stdout, stderr))
 
     # Create final directory path
-    if not os.path.exists(finalDir):
-        # mode = 0o755
-        # os.makedirs(finalDir, mode)
+    if os.path.exists(finalDir):
+        os.rmdir(finalDir)
+        logger.info('Remove directory '+finalDir+ '.')
         os.makedirs(finalDir, exist_ok=True)
-        logger.info('Made directory '+Path(finalDir).parts[-1]+ '.')
+        logger.info('Made directory '+finalDir+ '.')
     else:
-        logger.info('Directory '+Path(finalDir).parts[-1]+' already made.')
+       os.makedirs(finalDir, exist_ok=True)
+       logger.info('Directory '+finalDir+' does not exist so make it.')
 
-    # Move cog to final directory
-    shutil.move(inputDir+outputFile, finalDir+outputFile)
-    logger.info('Moved cog file to '+Path(finalDir).parts[-1]+' directory.')
+    # Move cogs to final directory
+    for finalPathFile in glob.glob(inputDir+'*.cog.tif'):
+        try:
+            shutil.move(finalPathFile, finalDir)
+            logger.info('Moved cog file '+finalPathFile.split("/")[-1]+' to '+finalDir+' directory.')
+        except OSError as err:
+            logger.error('Failed to move cog file '+finalPathFile.split("/")[-1]+' to '+finalDir+' directory.')
+            sys.exit(1)
 
 @logger.catch
 def main(args):
     # Get input variables from args
-    inputFile = args.inputFile 
+    inputParam = os.path.join(args.inputParam, '')
     inputDir = os.path.join(args.inputDir, '')
     finalDir = os.path.join(args.finalDir, '')
-
+    inputDir = os.path.join(inputDir+inputParam, '')
+    finalDir = os.path.join(finalDir+inputParam, '')
+    
     # Remove old logger and start new logger
     logger.remove()
     log_path = os.path.join(os.getenv('LOG_PATH', os.path.join(os.path.dirname(__file__), 'logs')), '')
     logger.add(log_path+'geotiff2cog.log', level='DEBUG')
 
     # Check if input file exists and if it does run geotiff2cog function 
-    if os.path.exists(inputDir+inputFile):
+    if os.path.exists(inputDir):
         # When error exit program
         logger.add(lambda _: sys.exit(1), level="ERROR")
 
-        logger.info('Create cog file from '+inputFile.strip()+' tiff file.')
+        logger.info('Create cog files in '+inputDir.strip()+' tiff file.')
 
-        geotiff2cog(inputFile, inputDir, finalDir)
+        geotiff2cog(inputDir, finalDir)
 
-        logger.info('Created cog file.')
- 
+        logger.info('Created cog files in '+inputDir.strip()+'.')
+
+        # If timeseries create meta files for mozaic
+        if finalDir.split('/')[-2].lower().find('max') == -1:
+            logger.info('Create meta file for timeseries mosaic COGs')
+            f = open(finalDir+'indexer.properties', 'w')
+            f.write('TimeAttribute=ingestion\nElevationAttribute=elevation\nSchema=*the_geom:Polygon,location:String,ingestion:java.util.Date,elevation:Integer\nPropertyCollectors=TimestampFileNameExtractorSPI[timeregex](ingestion)\n')
+            f.close()
+
+            f = open(finalDir+'timeregex.properties', 'w')
+            f.write('regex=[0-9]{8}T[0-9]{6}\n')
+            f.close()
+
+            f = open(finalDir+'datastore.properties', 'w')
+            f.write('SPI=org.geotools.data.postgis.PostgisNGDataStoreFactory\nhost=localhost\nport=5432\ndatabase=apsviz_cog_mosaic\nschema=public\nuser=apsviz_cog_mosaic\npasswd=cog_mosaic\nLoose\ bbox=true\nEstimated\ extends=false\nvalidate\ connections=true\nConnection\ timeout=10\npreparedStatements=true\n')
+            f.close()
+
+            # Zip finalDir into zip file, and then remove the finalDir
+            logger.info('Zip finalDir '+finalDir)
+
+            try:
+                shutil.make_archive(finalDir[:-1], 'zip', root_dir="/".join(finalDir.split('/')[:-2]), base_dir=finalDir.split('/')[-2])
+                logger.info('Ziped finalDir to FinalDir to zip file '+"/".join(finalDir.split('/')[:-2])+'/'+finalDir.split('/')[-2]+'.zip')
+            except OSError as err:
+                logger.error('Problem zipping file '+"/".join(finalDir.split('/')[:-2])+'/'+finalDir.split('/')[-2]+'.zip')
+                sys.exit(1)
+
+            try:
+                shutil.rmtree(finalDir)
+                logger.info('Removed finalDir '+finalDir)
+            except OSError as err:
+                logger.error('Problem removing finalDir '+finalDir)
+                sys.exit(1)
+
+        else:
+            logger.info('Data is not timeseries so no need to create meta file')
+
+            # Zip finalDir into zip file, and then remove the finalDir
+            logger.info('Zip finalDir '+finalDir)
+
+            try:
+                shutil.make_archive(finalDir[:-1], 'zip', root_dir="/".join(finalDir.split('/')[:-2]), base_dir=finalDir.split('/')[-2])
+                logger.info('Ziped finalDir to FinalDir to zip file '+"/".join(finalDir.split('/')[:-2])+'/'+finalDir.split('/')[-2]+'.zip')
+            except OSError as err:
+                logger.error('Problem zipping file '+"/".join(finalDir.split('/')[:-2])+'/'+finalDir.split('/')[-2]+'.zip')
+                sys.exit(1)
+
+            try:
+                shutil.rmtree(finalDir)
+                logger.info('Removed finalDir '+finalDir)
+            except OSError as err:
+                logger.error('Problem removing finalDir '+finalDir)
+                sys.exit(1)
+
     else:
-        logger.info(inputFile+' does not exist')
-        sys.exit(0)
+        logger.info(inputDir+' does not exist')
+        sys.exit(1)
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
     parser = argparse.ArgumentParser()
 
     # Optional argument which requires a parameter (eg. -d test)
-    parser.add_argument("--inputFILE", "--inputFile", help="Input file name", action="store", dest="inputFile")
+    parser.add_argument("--inputPARAM", "--inputParam", help="Input parameter", action="store", dest="inputParam")
     parser.add_argument("--inputDIR", "--inputDir", help="Input directory path", action="store", dest="inputDir")
     parser.add_argument("--finalDIR", "--finalDir", help="Final directory path", action="store", dest="finalDir")
 
